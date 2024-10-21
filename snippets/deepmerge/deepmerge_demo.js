e9b27f0fe87349f848da2a262b1a944a25b7b093
Nikolai Maltsev <nmaltsev@acri-st.fr>
2024-10-21 14:02:00 +0200

    update deepmerge demo

--- snippets/deepmerge/deepmerge_demo.js
+++ snippets/deepmerge/deepmerge_demo.js
@@ -1,3 +1,28 @@
 import {deepmerge} from './deepmerge.mjs'
 
-console.log(deepmerge);
+(function(){
+    const   $form = document.getElementById('form'),
+            $json = document.getElementById('json'),
+            $updates = document.getElementById('updates'),
+            $results = document.getElementById('results');
+    
+    $form.addEventListener('submit', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        const originalDoc = saveParse(e.target.elements.json.value)
+        const updatesDoc = saveParse(e.target.elements.updates.value)
+
+        deepmerge(originalDoc, updatesDoc)
+        $results.textContent = JSON.stringify(originalDoc, null, '\t')
+    })
+    $json.value = JSON.stringify({options: [{id: 1, value:'item1'}, {id: 2, value:'item2'}]}, null, '\t')
+    $updates.value = JSON.stringify({options: [{id: 3, value:'item3'}]}, null, '\t')
+}());
+
+function saveParse(str) {
+    try {
+        return JSON.parse(str)
+    } catch(e) {
+        return null;
+    }
+}
